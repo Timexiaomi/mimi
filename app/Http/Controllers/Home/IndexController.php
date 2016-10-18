5d08c36018aff621323e76bb327d5ed394f29b13
hyh <634152000@qq.com>
2016-10-18 15:10:15 +0800

    10

---
+++ app/Http/Controllers/Home/IndexController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+
+    //跳转的前台主页的方法
+	public function index()
+	{
+
+                    $class=\DB::table('goodsclass')->get();
+                    $goods=\DB::table('goods')->where('goods_ground','=','1')->get();
+		return view('home.index',['db'=>$goods,'class'=>$class]);
+	}
+        //头部导航栏控制
+        public function head(Request $request)
+        {
+                $id=$request->data;
+                $ob=\DB::table('goods')->where('cid','=',$id)->get();
+                return json_encode($ob);
+        }
+
+        //商品信息页面
+        public function goodsmessage($id){
+                    // $version=\DB::table('goodsdata')->join('goods','goodsdata.gid','=','goods.id')->where('goodsdata.gid','=',$id)->get();
+                    $good=\DB::table('goods')->where('id','=',$id)->first();
+                    $version=\DB::table('goodsdata')->where('gid','=',$id)->get();
+                    // dd($version);
+
+                    // dd($good);
+                    $class=\DB::table('goodsclass')->get();
+                    $goods=\DB::table('goods')->get();
+            return view('home.goodsmessage',['db'=>$goods,'class'=>$class,'ob'=>$version,'good'=>$good]);
+        }
+        //商品版本颜色选择
+        public function color(Request $request)
+        {
+                $id=$request->data;
+                $color=\DB::table('goodsimage')->where('gdid','=',$id)->get();
+
+                return json_encode($color);
+        }
+
+        //购物车页面
+        public function cart(){
+
+            $class=\DB::table('goodsclass')->get();
+            $goods=\DB::table('goods')->get();
+            return view('home.cart',['db'=>$goods,'class'=>$class]);
+        }
+        //订单页面
+        public function order(){
+            $class=\DB::table('goodsclass')->get();
+            $goods=\DB::table('goods')->get();
+            return view('home.order',['db'=>$goods,'class'=>$class]);
+        }
+        //支付页面
+        public function pay(){
+            $class=\DB::table('goodsclass')->get();
+            $goods=\DB::table('goods')->get();
+            return view('home.pay',['db'=>$goods,'class'=>$class]);
+        }
+        //用户信息页面
+        public function user(){
+            $class=\DB::table('goodsclass')->get();
+            $goods=\DB::table('goods')->get();
+            return view('home.user',['db'=>$goods,'class'=>$class]);
+        }
+        //搜索信息页面
+        public function search(){
+            $class=\DB::table('goodsclass')->get();
+            $goods=\DB::table('goods')->get();
+            return view('home.search',['db'=>$goods,'class'=>$class]);
+        }
+        //商品详情获取
+        public function goodsgata(Request $request){
+                $id=$request->data;
+                $ob=\DB::table('goods')->where('id','=',$id)->first();
+                return json_encode($ob);
+        }
+        //个人地址管理
+        public function address(){
+                $class=\DB::table('goodsclass')->get();
+            $goods=\DB::table('goods')->get();
+            return view('home.address',['db'=>$goods,'class'=>$class]);
+
+        }
+
+}
---
+++ resources/views/home/address.blade.php
@@ -0,0 +1,53 @@
+@extends('home/base/message')
+@section('message')
+<div class="span16">
+    <div class="uc-box uc-main-box">
+        <div class="uc-content-box">
+            <div class="box-hd">
+                <h1 class="title">收货地址</h1>
+                <!--
+                <div class="more clearfix">
+                    <ul class="filter-list J_addrType">
+                        <li class="first active"><a href="http://order.mi.com/user/address?r=66924.1476759002">普通收货地址</a></li>
+                        <li class=""><a href="http://order.mi.com/user/address?type=tv&r=66924.1476759002">大型商品收货地址</a></li>
+                    </ul>
+                </div>
+                -->
+            </div>
+            <div class="box-bd">
+
+                <div class="user-address-list J_addressList clearfix">
+                    <div class="address-item address-item-new" data-type="" id="J_newAddress">
+                        <i class="iconfont"></i>
+                        添加新地址
+                    </div>
+
+                            <div class="address-item J_addressItem" data-address_id="10160929578802829" data-consignee="韩宇航" data-tel="138****5929" data-province_id="17" data-province_name="河南" data-city_id="197" data-city_name="许昌市" data-district_id="1907" data-district_name="长葛市" data-area="1907002" data-zipcode="461500" data-address="韩庄村130号" data-tag_name="家">
+                                <dl>
+                                    <dt>
+
+                                                                                    <span class="tag">家</span>                                        <em class="uname">韩宇航</em>
+                                    </dt>
+                                    <dd class="utel">138****5929</dd>
+                                    <dd class="uaddress">河南 许昌市 长葛市                                        董村镇                                        <br>韩庄村130号 (461500)</dd>
+                                </dl>
+                                <div class="actions">
+                                    <a class="modify J_addressModify" data-id="10160929578802829" href="javascript:void(0);" data-stat-id="05bdb261c37afc34" onclick="_msq.push([&#39;trackEvent&#39;, &#39;73ce9fb1e71baa17-05bdb261c37afc34&#39;, &#39;javascript:void(0);&#39;, &#39;pcpid&#39;]);">修改</a>
+                                    <a class="modify J_addressDel" data-id="10160929578802829" data-stat-id="2a7ebd088f434839" onclick="_msq.push([&#39;trackEvent&#39;, &#39;73ce9fb1e71baa17-2a7ebd088f434839&#39;, &#39;&#39;, &#39;pcpid&#39;]);">删除</a>
+                                </div>
+                             </div>
+                                            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="J_modalEditAddress" class="modal fade modal-hide modal-edit-address">
+    <div class="modal-body">
+        <iframe width="100%" height="100%" frameborder="0" src=""></iframe>
+    </div>
+</div>
+        </div>
+    </div>
+</div>
+@endsection
